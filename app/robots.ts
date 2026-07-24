6026764692f7e884bfc6bd8357853109742a47f4
ai-x-systems <aixsautomation@gmail.com>
2026-07-25 01:14:04 +0500

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -3,6 +3,6 @@ import type { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: "*", allow: "/" },
-    sitemap: "https://paddlex-pakistan.vercel.app/sitemap.xml",
+    sitemap: "thepadelx.vercel.app/sitemap.xml",
   };
 }
